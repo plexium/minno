518c97f26bb9e96476d280accb40706def676f5b
benglish <benglish@e26cb320-6b0a-0410-892d-e6db4bb496c4>
2010-09-20 18:15:53 +0000

    - added breadcrumb feature

---
+++ com.bluelinecity.minno/trunk/source/function.breadcrumb.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+ * Function: breadcrumb
+ * Prints out a linked breadcrumb trail
+ * Usage: <minno:breadcrumb params="seperator,path|text,path,..." />
+ */
+
+function breadcrumb( )
+{
+   $args = func_get_args();
+   $buffer = '';
+   
+   $sep = ( count($args) >= 1 ) ? array_shift($args) : '&gt;';
+
+   //try and guess the path from url
+   if ( count($args) == 0 )
+   {      
+      $id = $GLOBALS['id'];
+      //$id = 'products/vaccums/dirtdevel.html';
+      $parts = explode('/', $id );
+      
+      $args[] = 'index.html|Home';
+      
+      foreach ( $parts as $index => $part )
+      {
+         if ( $part != basename($id) ) 
+         {
+            $args[] = implode('/', array_slice($parts, 0, $index+1 ) ) . '/index.html|' . 
+            ucwords( preg_replace('/[\-\_]/',' ',$part));
+         }
+         else if ( $part != 'index.html' )
+         {
+            $args[] = $id;
+         }
+      }              
+   }   
+   
+   foreach ( $args as $arg )
+   {
+      list( $link, $text ) = explode('|',$arg);
+      $buffer .= ( empty($buffer)? '' : $sep );
+      $text   = (empty($text) ? ucwords( preg_replace('/[\-\_]/',' ',basename($link,'.html')) ) : $text );
+      $buffer .= ' <a href="'.htmlspecialchars($link).'">'. htmlspecialchars($text) .'</a> ';
+   }
+    
+   return $buffer;
+}
